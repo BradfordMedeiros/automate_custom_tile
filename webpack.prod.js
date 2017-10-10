b8678d8493136c7ea2c7d6973632be08c11b06cf
BradfordMedeiros <bradmedeiros0@gmail.com>
2017-10-09 20:36:00 -0700

    initial commit

---
+++ webpack.prod.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const process = require('process');
+const webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: ["babel-polyfill", "./src/index.js"],
+  output: {
+    path: path.join(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/static/',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css/,
+        loader: 'style!css',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'es2015', 'stage-0'],
+
+        }
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.UglifyJsPlugin({
+      minimize: true
+    }),
+    new webpack.IgnorePlugin(/^\.\/locale$/, [/moment$/]),
+    new webpack.NoErrorsPlugin(),
+  ],
+};
